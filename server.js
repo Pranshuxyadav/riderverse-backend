333e87262e75b4ec4fcd7659375bdd26b723df37
Pranshu Yadav <107531073+Pranshuxyadav@users.noreply.github.com>
2026-03-23 17:56:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -193,6 +193,4 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log('Server listening on http://localhost:' + PORT);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
